4e00ea05ac9476d02a40c4ea13e9703429c57411
Adrien Dubois <adrien.dubois.be@gmail.com>
2017-03-04 19:19:17 +0100

    AddRestaurantToReviews - Foreign key created in reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303192153) do
+ActiveRecord::Schema.define(version: 20170304181332) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -23,8 +23,10 @@ ActiveRecord::Schema.define(version: 20170303192153) do
   create_table "reviews", force: :cascade do |t|
     t.text     "content"
     t.integer  "rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "restaurant_id"
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
 end
