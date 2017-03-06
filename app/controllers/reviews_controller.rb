1e270659c0a8f3a35980d373164a423384f7f63a
Adrien Dubois <adrien.dubois.be@gmail.com>
2017-03-06 19:42:52 +0100

    Can add a review to a restaurant

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,17 @@
 class ReviewsController < ApplicationController
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id]) # we need @restaurant in our `simple_form_for`
+    @review = Review.new
+  end
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = Restaurant.find(params[:restaurant_id]) # we need `restaurant_id` to asssociate review with corresponding restaurant
+    @review.save
+    redirect_to restaurant_path(@review.restaurant_id)
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews, :dependent => :destroy
+  has_many :reviews, dependent: :destroy
   validates :name, uniqueness: true, presence: true # Only one same name, name mandatory
   validates :address, presence: true # Address field mandatory
   validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false }
